097bbe546b8fd107329b31c166641d9b00421599
AleksanderBjelk <bjelkaleksander@gmail.com>
2023-12-01 11:32:14 +0100

    Save button tillagd och fungerande

--- demo/src/main/java/com/example/demo/TableController.java
+++ demo/src/main/java/com/example/demo/TableController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.TableView;
 import com.eclipsesource.json.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 import static com.example.demo.TableReader.scene;
@@ -26,7 +28,77 @@ public class TableController {
     @FXML
     void onSaveButton(ActionEvent event) {
 
+        System.out.println("The user is saving a file."); // or something else
+        FileChooser fileC = new FileChooser();
+        fileC.setInitialDirectory(new File("src/main/java/com/example/demo")); // init path annars C
+        fileC.setTitle("Save File");
+
+        fileC.getExtensionFilters().addAll(
+                // new FileChooser.ExtensionFilter("TABLE FILES", "*.csv", "*.json"),
+                new FileChooser.ExtensionFilter("CSV", "*.csv"),
+                new FileChooser.ExtensionFilter("Json", "*.json"),
+                new FileChooser.ExtensionFilter("ALL FILES", "*.*")
+        );
+
+        File file = fileC.showSaveDialog(scene.getWindow());
+        if (file != null) {
+            System.out.println(file.getPath());
+            writeFile(file);
+        } else  {
+            System.out.println("save canceled!"); // or something else
+        }
     }
+
+    private void writeFile(File file) {
+        try {
+            System.out.println(file.getName());
+            String[] sArray= file.getName().split("\\.");
+
+            //String  fileExtention = file.getName().split("\\.")[1];
+            String  fileExtention = file.getName().split("\\.")[sArray.length-1];
+            System.out.println(fileExtention);
+
+            file.createNewFile(); //empty file med namn
+
+             if(fileExtention.equals("csv")){
+
+            } else if (fileExtention.equals("json")) {
+
+            }
+
+            switch (fileExtention) {
+                case "json":
+                    System.out.println("Json file");
+                    break;
+                case "csv":
+                    System.out.println("CSV file");
+                    break;
+                default:
+                    System.out.println("Other files");
+            }
+
+            switch (fileExtention){
+                case "csv"-> {
+                    System.out.println("Please wirte CSV here:");
+                    PrintWriter w = new PrintWriter(file);
+                    w.println("Förnamn,Efternamn,age");
+                    w.close();
+                }
+                case "json"-> System.out.println("Please write Json here:");
+                case "txt"-> {
+                    //fyller i data
+                    PrintWriter writer = new PrintWriter(file.getPath(), "UTF-8");
+                    writer.println("detta är en text fil");
+                    writer.println("by: Aleksander Bjelk, credit due to Alrik He for the code.");
+                    writer.close();
+                }
+            }
+        }catch (IOException io){
+            System.out.println(io);
+        }
+    }
+
+
 
     @FXML
     String selectFileFromExplorer(ActionEvent event) {
@@ -61,6 +133,7 @@ public class TableController {
             for (String cName : columnNames.names()){
                 String val = columnNames.get(cName).asString();
                 Table.getColumns().add(new TableColumn<>(val));
+
 
 
             }
